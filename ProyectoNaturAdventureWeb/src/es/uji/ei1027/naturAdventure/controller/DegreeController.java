dbeeafa75aa2d12c03399e963a569a4a2cd4e111
david <david@david-System-Product-Name>
2015-04-23 20:19:35 +0200

    first degree update method

--- ProyectoNaturAdventureWeb/src/es/uji/ei1027/naturAdventure/controller/DegreeController.java
+++ ProyectoNaturAdventureWeb/src/es/uji/ei1027/naturAdventure/controller/DegreeController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -41,6 +42,12 @@ public class DegreeController {
 		}
 		degreeDao.addDegree( degree );
 		return "redirect: list.html";
+	}
+	
+	@RequestMapping(value="/update/{codDegree}")
+	public String updateDegree( @PathVariable int codDegree, Model model ) {
+		model.addAttribute( "degree", degreeDao.getDegree( codDegree ) );
+		return "degree/update";
 	}
 	
 }
